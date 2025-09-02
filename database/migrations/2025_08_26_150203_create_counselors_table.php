cfe5af8ef58a2f79a1f0d2ee84b66164b376be0f
mscopa <maurocopa20@gmail.com>
2025-09-02 16:12:22 -0300

    delete: delete user_id column from counselors table

--- database/migrations/2025_08_26_150203_create_counselors_table.php
+++ database/migrations/2025_08_26_150203_create_counselors_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('counselors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
             $table->foreignId('company_id')->constrained('companies');
             $table->string('first_name');
             $table->string('middle_name')->nullable();
